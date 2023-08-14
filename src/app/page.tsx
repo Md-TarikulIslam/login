fc930f0ffd8b0b0dcae903f80bb25274f4787bd9
Md-TarikulIslam <nayon1820@gmail.com>
2023-08-14 12:35:43 +0600

    14th Aug, 2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import FeaturedProducts from './components/Products/FeaturedProducts'
 
 export default function Home() {
   return (
-    <div className='max-w-screen-2xl mx-auto min-h-screen'>
+    <div className='max-w-screen-2xl mx-auto min-h-screen pb-4'>
       <Navbar />
       <div>
         <FeaturedBio />
