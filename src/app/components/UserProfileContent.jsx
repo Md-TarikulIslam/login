3bee817849bf62d2a9737c1d03199015cdf89a02
MdTarikulIslam <nayon1820@gmail.com>
2023-08-15 17:23:42 +0600

    15th Aug, 2023

--- src/app/components/UserProfileContent.jsx
+++ src/app/components/UserProfileContent.jsx
@@ -13,11 +13,24 @@ const UserProfileContent = ({ select, data }) => {
 export default UserProfileContent;
 
 const Profile = ({ data }) => {
-    
+    const { fullname, isVerified, email } = data
+
     return (
         <div className='flex justify-center items-center w-full'>
             <div>
-                <h2>Here is the profile data show</h2>
+                <h2 className='text-xl mb-5'>Here is the profile data show</h2>
+                <div className="bg-white shadow-md rounded-md p-6">
+                    <h2 className="text-2xl font-semibold text-center text-blue-600">{fullname}</h2>
+                    <div className="">
+                        <p className="text-center">
+                            <strong>Email:</strong> {email}
+                        </p>
+                    </div>
+                    <div className='flex items-start gap-1 justify-center'>
+                        <strong className="">Verified: </strong>
+                        {isVerified ? <div className='text-black'>Yes</div> : <div className='text-red-700'>No</div>}
+                    </div>
+                </div>
             </div>
         </div>
     )
